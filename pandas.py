9e002cdf8fe058d53e74a0db5d89f9b5663e148e
Mikail1010 <53097834+Mikail1010@users.noreply.github.com>
2021-05-15 21:31:48 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -2,4 +2,9 @@
 #  Useful for filling read data files having merged cells
 
 df['column'] = pd.Series(df['column']).fillna(method='ffill')
+
+#One value of column which applies for multiple values of other column - converting that into a dict mapped with values
+dict = {}
+for i in df['Column1'].unique():
+  dcit[i] = list(df[df['Column1'] == i]['Column2'].values)
   
